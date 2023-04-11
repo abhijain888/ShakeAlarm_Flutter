a085241fa61cc1b6ab4d9d192bcf2ac2ce0240dd
Abhishek Jain <abhishek.2022@tecorb.co>
2023-04-11 13:28:40 +0530

    qoute implemented with bloc

--- lib/bloc/qoute_bloc/qoute_bloc.dart
+++ lib/bloc/qoute_bloc/qoute_bloc.dart
@@ -10,8 +10,10 @@ class QouteBloc extends Bloc<QouteEvent, QouteState> {
   Random random = Random();
   QouteBloc()
       : super(QouteState(
-          backColor: Colors.blueAccent[100]!,
-          cardColor: Colors.blueAccent[400]!,
+          color: {
+            "backColor": Colors.blueAccent[100]!,
+            "cardColor": Colors.blueAccent[400]!,
+          },
           qoute: Qoute(
             qoute:
                 "Dream is not that which you see while sleeping it is something that does not let you sleep.",
@@ -23,22 +25,52 @@ class QouteBloc extends Bloc<QouteEvent, QouteState> {
       emit(
         QouteState(
           qoute: qoutes[index],
-          backColor: colors[index],
-          cardColor: cardColors[index],
+          color: colors[index],
         ),
       );
     });
   }
 
-  List<Color> colors = [
-    Colors.blueAccent[100]!,
-    Colors.purple[100]!,
-    Colors.teal[100]!,
-    Colors.red[100]!,
-    Colors.purple[100]!,
-    Colors.cyan[100]!,
-    Colors.blueGrey[100]!,
+  List<Map<String, Color>> colors = [
+    {
+      "backColor": Colors.blueAccent[100]!,
+      "cardColor": Colors.blueAccent[400]!,
+    },
+    {
+      "backColor": Colors.purple[100]!,
+      "cardColor": Colors.purple[400]!,
+    },
+    {
+      "backColor": Colors.red[100]!,
+      "cardColor": Colors.red[400]!,
+    },
+    {
+      "backColor": Colors.pink[100]!,
+      "cardColor": Colors.pink[400]!,
+    },
+    {
+      "backColor": Colors.cyan[100]!,
+      "cardColor": Colors.cyan[400]!,
+    },
+    {
+      "backColor": Colors.blueGrey[100]!,
+      "cardColor": Colors.blueGrey[400]!,
+    },
+    {
+      "backColor": Colors.teal[100]!,
+      "cardColor": Colors.teal[400]!,
+    },
   ];
+
+  // List<Color> colors = [
+  //   Colors.blueAccent[100]!,
+  //   Colors.purple[100]!,
+  //   Colors.teal[100]!,
+  //   Colors.red[100]!,
+  //   Colors.pink[100]!,
+  //   Colors.cyan[100]!,
+  //   Colors.blueGrey[100]!,
+  // ];
 
   List<Color> cardColors = [
     Colors.blueAccent[400]!,
